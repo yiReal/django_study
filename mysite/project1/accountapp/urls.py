333219532bb50176936932d99ae99348502afe36
yireal <andytwo9878@gmail.com>
2023-08-11 18:17:10 +0900

    first_commit

---
+++ mysite/project1/accountapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from accountapp.views import hello_world
+
+app_name = "accountapp"
+
+urlpatterns = [
+    path('hello_world/',hello_world)
+]
