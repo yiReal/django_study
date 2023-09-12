2a48b6ae99f27acbfbfbb20f6e7e7f045cb15681
yireal <andytwo9878@gmail.com>
2023-09-12 20:54:32 +0900

    18강

--- mysite/project1/accountapp/views.py
+++ mysite/project1/accountapp/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
 from accountapp.models import HelloWorld
+from django.urls import reverse
 # Create your views here.
 
 def hello_world(request):
@@ -11,7 +12,8 @@ def hello_world(request):
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-        
-        return render(request,'accountapp/hello_world.html',context = {'hello_world_output' : new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request,'accountapp/hello_world.html',context = {'text' : 'GET_METHOD!'})
+        hello_world_list = HelloWorld.objects.all()
+        
+        return render(request,'accountapp/hello_world.html',context = {'hello_world_list' : hello_world_list})
